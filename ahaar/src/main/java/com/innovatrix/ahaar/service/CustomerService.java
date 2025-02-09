b453f7eb554929450d48368e3f4cdb840ed1f070
Swastika_Pandit <swastikapandit02@gmail.com>
2025-02-09 18:46:19 +0600

    removed unnecessary imports

--- ahaar/src/main/java/com/innovatrix/ahaar/service/CustomerService.java
+++ ahaar/src/main/java/com/innovatrix/ahaar/service/CustomerService.java
@@ -6,18 +6,14 @@ import com.innovatrix.ahaar.exception.UserNotFoundException;
 import com.innovatrix.ahaar.model.Customer;
 import com.innovatrix.ahaar.model.RefreshToken;
 import com.innovatrix.ahaar.repository.CustomerRepository;
-import com.innovatrix.ahaar.util.ResponseBuilder;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
