cf53c3b117cc102e0411a1f304831e28933be3eb
Swastika_Pandit <swastikapandit02@gmail.com>
2025-03-16 21:58:33 +0600

    added builder to CustomerDTO

--- ahaar/src/main/java/com/innovatrix/ahaar/dto/CustomerDTO.java
+++ ahaar/src/main/java/com/innovatrix/ahaar/dto/CustomerDTO.java
@@ -6,6 +6,8 @@ import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.RequiredArgsConstructor;
 
@@ -13,6 +15,8 @@ import java.util.Date;
 
 @Data
 @RequiredArgsConstructor
+@AllArgsConstructor
+@Builder
 public class CustomerDTO {
     @NotBlank(message = "Username is required")
     @Size(min = 3, max = 50, message = "Username must be between 3 and 50 characters")
