55c85192e361a777eb9d980077dc9c801d81e261
Swastika_Pandit <swastikapandit02@gmail.com>
2025-03-16 22:04:10 +0600

    added exception handler for DataIntegrityViolatedException and HTTPMessageNotReadableException

--- ahaar/src/main/java/com/innovatrix/ahaar/exception/GlobalExceptionHandler.java
+++ ahaar/src/main/java/com/innovatrix/ahaar/exception/GlobalExceptionHandler.java
@@ -2,8 +2,11 @@ package com.innovatrix.ahaar.exception;
 
 import com.innovatrix.ahaar.model.APIResponse;
 import com.innovatrix.ahaar.util.ResponseBuilder;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.FieldError;
 import org.springframework.validation.ObjectError;
@@ -17,8 +20,19 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @ControllerAdvice
 public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DataIntegrityViolationException.class)
+    public ResponseEntity<APIResponse<Void>> handleDataIntegrityViolationException(WebRequest request, DataIntegrityViolationException ex) {
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(ResponseBuilder.error(HttpStatus.CONFLICT.value(), "Data integrity violated", null));
+    }
+
+    @ExceptionHandler(HttpMessageNotReadableException.class)
+    public ResponseEntity<APIResponse<Void>> handleHttpMessageNotReadableException(WebRequest request, HttpMessageNotReadableException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ResponseBuilder.error(HttpStatus.BAD_REQUEST.value(), ex.getMessage(), null));
+    }
 
     @ExceptionHandler(BindException.class)
     public ResponseEntity<APIResponse<ArrayList<String>>> handleInvalidArgumentException(BindException ex, WebRequest request) {
